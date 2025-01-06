52dd795319069114f1d5340e5d14a84d976c0610
WWYDF <89052946+WWYDF@users.noreply.github.com>
2025-01-05 20:02:47 -0500

    fix
    idk what happened tbh

---
+++ src/main/java/net/devante/spice/Spice.java
@@ -0,0 +1,17 @@
+package net.devante.spice;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Spice extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
